304a09219f43edbafb4c8124ed56390551b315ee
mmz-001 <70096033+mmz-001@users.noreply.github.com>
2022-07-10 09:02:41 +0530

    Complete instruction decoder and sim

--- sources_1/new/INS_DECODER.vhd
+++ sources_1/new/INS_DECODER.vhd
@@ -1,9 +1,10 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
 
 entity INS_DECODER is
   Port (
-    I : in STD_LOGIC_VECTOR (12 downto 0);
+    I : in STD_LOGIC_VECTOR (11 downto 0);
     R : in STD_LOGIC_VECTOR (3 downto 0);
     R_En, RA_Sel, RB_Sel, JMP_Add : out STD_LOGIC_VECTOR (2 downto 0);
     Load_Sel, Add_Sub_Sel, JMP_Flag : out STD_LOGIC;
@@ -14,21 +15,22 @@ end INS_DECODER;
 architecture Behavioral of INS_DECODER is
 
   signal Ins : STD_LOGIC_VECTOR (1 downto 0);
-  signal RA, RB : STD_LOGIC_VECTOR (2 downto 0);
   
 begin
+  Ins <= I(11 downto 10);
 
-  Ins <= I(1 downto 0);
-  JMP_Flag <= '0';
-  RA <= I(4 downto 2);
-  RB <= I(5 downto 7);
-  JMP_Add <= I(11 downto 8);
-  Im_Val <= I(11 downto 8);
-
-  process (INS, R) 
+  process (I, Ins, R) 
   begin
-    R_En <= I(4 downto 2);
 
+    JMP_Flag <= '0';
+    JMP_Add <= I(2 downto 0);
+    Im_Val <= I(3 downto 0);
+    R_En <= I(9 downto 7);
+    RA_Sel <= I(9 downto 7);
+    RB_Sel <= I(6 downto 4);
+    Load_Sel <= '0';
+    Add_Sub_Sel <= '0';
+    
     if Ins = "10" then
       -- MOVI R, d
       Load_Sel <= '0';
@@ -36,20 +38,16 @@ begin
     elsif Ins = "00" then
       -- ADD Ra, Rb 
       Load_Sel <= '1';
-      RA_Sel <= RA;
-      RB_Sel <= RB;
       Add_Sub_Sel <= '0';
 
     elsif Ins = "01" then
       -- NEG R
       Load_Sel <= '1';
-      RA_Sel <= "0000";
-      RB_Sel <= RA;
+      RB_Sel <= "000";
       Add_Sub_Sel <= '1';
 
     elsif Ins = "11" then
       -- JZR R, d 
-      R_En <= "000";
 
       if R = "0000" then
         JMP_Flag <= '1';
