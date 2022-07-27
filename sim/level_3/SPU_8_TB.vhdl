6d93dabdc9aeda3a62fba9a61bf5148820956325
mmz-001 <70096033+mmz-001@users.noreply.github.com>
2022-07-27 11:23:47 +0530

    Modify SPU_8 and Counter_2

---
+++ sim/level_3/SPU_8_TB.vhdl
@@ -0,0 +1,53 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity SPU_8_TB is
+end SPU_8_TB;
+
+architecture Behavioral of SPU_8_TB is
+    component SPU_8
+        Port ( I : in STD_LOGIC_VECTOR (7 downto 0);
+        Flags : in STD_LOGIC_VECTOR (3 downto 0);
+        Clk : in STD_LOGIC;
+        Seven_Seg_Out_L : out STD_LOGIC_VECTOR (6 downto 0);
+        Anode_Out_L : out STD_LOGIC_VECTOR (3 downto 0));
+    end component;
+
+    signal Clk : STD_LOGIC := '0';
+    signal Flags, Anode_Out_L : STD_LOGIC_VECTOR(3 downto 0);
+    signal Seven_Seg_Out_L : STD_LOGIC_VECTOR(6 downto 0);
+    signal I : STD_LOGIC_VECTOR(7 downto 0);
+    
+begin
+    UUT : SPU_8
+    Port Map (
+        Clk => Clk,
+        Flags => Flags,
+        Anode_Out_L => Anode_Out_L,
+        I => I,
+        Seven_Seg_Out_L => Seven_Seg_Out_L
+    );
+
+    process
+    begin
+        Clk <= NOT Clk;   
+        wait for 20 ns;     
+    end process;
+
+    process
+    begin
+        Flags <= "0000";
+        I <= "00000010";  -- 2
+        wait for 400 ns;
+
+        I <= "00010000"; -- 16
+        wait for 400 ns;
+
+        I <= "11111111"; -- -1
+        wait for 400 ns;
+
+        I <= "01111111"; -- 127
+        wait;
+    end process;
+end Behavioral;
--- src/level_1/Counter_2.vhdl
+++ src/level_1/Counter_2.vhdl
@@ -4,7 +4,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity Counter_2 is
     Port( Clk : in STD_LOGIC;
-          Q : out STD_LOGIC_VECTOR(1 downto 0));
+          Q : out STD_LOGIC_VECTOR(1 downto 0) := "00");
 end Counter_2;
 
 architecture Behavioral of Counter_2 is
@@ -16,7 +16,8 @@ begin
     begin
         if (rising_edge(Clk)) then
             Count <= (Count + 1) MOD 4;
+            Q <= STD_LOGIC_VECTOR(to_unsigned(Count, Q'length));
         end if;
-        Q <= STD_LOGIC_VECTOR(to_unsigned(Count, Q'length));
+        
     end process;
 end Behavioral;
