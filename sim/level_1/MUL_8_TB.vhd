e6ec28545f8f4f9d5b2416855521678ae9064946
Isuru Gunarathne <isuru623@gmail.com>
2022-08-08 19:07:36 +0530

    Add on/off functionality

--- sim/level_1/MUL_8_TB.vhd
+++ sim/level_1/MUL_8_TB.vhd
@@ -9,23 +9,29 @@ architecture Behavioral of MUL_8_TB is
 component MUL_8
     Port ( A : in STD_LOGIC_VECTOR (7 downto 0);
            B : in STD_LOGIC_VECTOR (7 downto 0);
+           Mul_Sel : in STD_LOGIC;
            Q : out STD_LOGIC_VECTOR (7 downto 0);
            FLAG : out STD_LOGIC_VECTOR (3 downto 0));
 end component;
 
 signal a,b,q : STD_LOGIC_VECTOR (7 downto 0);
 signal flg : STD_LOGIC_VECTOR (3 downto 0);
+signal sel : STD_LOGIC;
 
 begin
 UUT : MUL_8 
 port map(
     A => a,
     B => b,
+    Mul_Sel => sel,
     Q => q,
     FLAG => flg);
 
 process
 begin
+
+sel <= '1'; -- enabling the multiplication unit
+
 a <= "00000000"; -- 0
 b <= "00000000"; -- 0
 wait for 100 ns;
@@ -57,6 +63,10 @@ wait for 100 ns;
 a <= "00110011"; -- 51
 b <= "00001010"; -- 10
 wait for 100 ns;
+
+sel <= '0'; -- turning off
+wait for 100 ns;
+sel <= '1'; -- turning on again
 
 wait;
 end process;
--- src/level_1/MUL_8.vhd
+++ src/level_1/MUL_8.vhd
@@ -5,6 +5,7 @@ use IEEE.numeric_std.all;
 entity MUL_8 is
     Port ( A : in STD_LOGIC_VECTOR (7 downto 0);
            B : in STD_LOGIC_VECTOR (7 downto 0);
+           Mul_Sel : in STD_LOGIC;
            Q : out STD_LOGIC_VECTOR (7 downto 0);
            FLAG : out STD_LOGIC_VECTOR (3 downto 0));
 end MUL_8;
@@ -21,13 +22,18 @@ Q_Buffer <= STD_LOGIC_VECTOR(signed(A) * signed(B));
 Q_out <= Q_Buffer(7 downto 0);
 Q_overflow <= Q_Buffer(15 downto 8);
 
-Q <= Q_out;
-
-FLAG (0) <= Q_overflow(0) xor Q_overflow(1) xor Q_overflow(2) xor Q_overflow(3) xor Q_overflow(4) xor Q_overflow(5) xor Q_overflow(6) xor Q_overflow(7); -- overflow
-FLAG (1) <= not(Q_out(0) or Q_out(1) or Q_out(2) or Q_out(3) or Q_out(4) or Q_out(5) or Q_out(6) or Q_out(7)); -- Zero 
-FLAG (2) <= Q_out(7); -- Negative 
-
-FLAG (3) <= '0'; -- Interrupt, have to map this to something. 
-
-
+process (Mul_Sel,Q_out) begin
+    
+    if Mul_Sel = '0' then
+        Q <= "00000000";
+        FLAG <= "0000";
+    else
+        Q <= Q_out;        
+        FLAG (0) <= Q_overflow(0) xor Q_overflow(1) xor Q_overflow(2) xor Q_overflow(3) xor Q_overflow(4) xor Q_overflow(5) xor Q_overflow(6) xor Q_overflow(7); -- overflow
+        FLAG (1) <= not(Q_out(0) or Q_out(1) or Q_out(2) or Q_out(3) or Q_out(4) or Q_out(5) or Q_out(6) or Q_out(7)); -- Zero 
+        FLAG (2) <= Q_out(7); -- Negative 
+        FLAG (3) <= '0'; -- Interrupt, have to map this to something. 
+    end if;
+    
+end process;
 end Behavioral;
