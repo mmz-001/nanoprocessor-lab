164f985d70daa5aa608cc11e34495b06ba218fcf
mmz-001 <70096033+mmz-001@users.noreply.github.com>
2022-08-15 05:11:24 +0530

    Finish LUT_16_16

--- src/level_1/LUT_16_16.vhd
+++ src/level_1/LUT_16_16.vhd
@@ -4,21 +4,28 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity LUT_16_16 is
     Port ( D : in STD_LOGIC_VECTOR (3 downto 0);
-           I : out STD_LOGIC_VECTOR (11 downto 0));
+           I : out STD_LOGIC_VECTOR (15 downto 0));
 end LUT_16_16;
 
 architecture Behavioral of LUT_16_16 is
-    type rom_type is array (0 to 7) of std_logic_vector(11 downto 0);
+    type rom_type is array (0 to 15) of std_logic_vector(15 downto 0);
         signal Instruction_ROM : rom_type :=(
-           "101110000000",-- MOVI R7, 0000
-           "100010000011",-- MOVI R1, 0011
-           "100100000001",-- MOVI R2, 0001
-           "010100000000",-- NEG R2
-           "001110010000",-- ADD R7, R1
-           "000010100000",-- ADD R1, R2
-           "110010000110",-- JZR R1 110
-           "110000000100" -- JZR R0 100
-            
+           "0010111000000000",-- MOVI R7, 0000
+           "0010111000000000",-- MOVI R1, 0011
+           "0010111000000000",-- MOVI R2, 0001
+           "0010111000000000",-- NEG R2
+           "0010111000000000",-- ADD R7, R1
+           "0010111000000000",-- ADD R1, R2
+           "0010111000000000",-- JZR R1 110
+           "0010111000000000",-- JZR R0 100
+           "0000000000000000",
+           "0000000000000000",
+           "0000000000000000",
+           "0000000000000000",
+           "0000000000000000",
+           "0000000000000000",
+           "0000000000000000",
+           "0000000000000000"            
         );  
 begin
     I <= Instruction_ROM(to_integer(unsigned(D)));
