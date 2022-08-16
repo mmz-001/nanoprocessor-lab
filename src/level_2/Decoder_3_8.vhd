c5790b93533ce0bd4cd522a5c679fc84f9083df8
PRO-GUNE <chathuragunasekera@gmail.com>
2022-08-16 13:32:33 +0530

    Modified working code

--- src/level_2/Decoder_3_8.vhd
+++ src/level_2/Decoder_3_8.vhd
@@ -9,37 +9,24 @@ entity Decoder_3_8 is
 end Decoder_3_8;
 
 architecture Behavioral of Decoder_3_8 is
-    component Decoder_2_4
-        port ( D : in STD_LOGIC_VECTOR (1 downto 0);
-               En : in STD_LOGIC;
-               Q : out STD_LOGIC_VECTOR (3 downto 0));
-    end component;
-    
-    signal D0,D1 : STD_LOGIC_VECTOR(1 downto 0);
-    signal Q0,Q1 : STD_LOGIC_VECTOR(3 downto 0);
-    signal En_0,En_1,D2 : STD_LOGIC;
-    
+    signal Q_sel : STD_LOGIC_VECTOR(7 downto 0);
+
 begin
 
-    Decoder_2_4_0 : Decoder_2_4
-    port map(
-        D=>D0,
-        En => En_0,
-        Q=>Q0);
-        
-    Decoder_2_4_1 : Decoder_2_4
-    port map(
-        D=>D1,
-        En => En_1,
-        Q => Q1);
-        
-    En_0 <= not(D(2)) and En; -- this is the En for the 3 to 8
-    En_1 <= D(2) and En;
-    
-    D0 <= D(1 downto 0);
-    D1 <= D(1 downto 0);
-    D2 <= D(2);
-    Q(3 downto 0) <= Q0;
-    Q(7 downto 4) <= Q1;
+    with D select Q_sel <=
+        "00000001" when "000",
+        "00000010" when "001",
+        "00000100" when "010",
+        "00001000" when "011",
+        "00010000" when "100",
+        "00100000" when "101",
+        "01000000" when "110",
+        "10000000" when "111",
+        (others => 'U') when others;
+
+    with En select Q <=
+		Q_sel       when '1',
+		"00000000"  when '0',
+		(others => 'U') when others;
 
 end Behavioral;
