1ab4ea60cbb619f2e1f1b27f05bb677bb841d42a
mmz-001 <70096033+mmz-001@users.noreply.github.com>
2022-08-17 14:26:17 +0530

    Modify DFF_4

--- src/level_1/DFF_4.vhd
+++ src/level_1/DFF_4.vhd
@@ -2,10 +2,10 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity DFF_4 is
-    Port ( D : in STD_LOGIC;
+    Port ( D : in STD_LOGIC_VECTOR(3 downto 0);
            Res : in STD_LOGIC;
            Clk : in STD_LOGIC;
-           Q : out STD_LOGIC);
+           Q : out STD_LOGIC_VECTOR(3 downto 0));
 end DFF_4;
 
 architecture Behavioral of DFF_4 is
@@ -14,7 +14,7 @@ begin
     process (Clk) begin
         if (rising_edge(Clk)) then
             if Res = '1' then
-                Q <= '0';
+                Q <= "0000";
             else
                 Q <= D;
             end if;
